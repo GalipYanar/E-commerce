8e68ff0e5f96de47fa64d4dae25e8e6fbcf1fd00
GalipYanar <qalipynrr@gmail.com>
2024-07-30 14:36:37 +0300

    bir adet category eklendi, product ekleme hatası fixlendi, bir adet product(resimli) eklendi

--- src/main/java/com/example/E_commerce/controller/ProductController.java
+++ src/main/java/com/example/E_commerce/controller/ProductController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -22,9 +19,9 @@ public class ProductController {
     private ProductService productService;
 
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Product> createProduct(@RequestPart("file")MultipartFile file,
-                                                 @RequestPart("product") Product product){
+                                                 @ModelAttribute Product product){
         return new ResponseEntity<>(productService.createProduct(file, product), HttpStatus.CREATED);
     }
 }
