4dd5cb07e82d69b370deffdae49a4c1ea3328703
GalipYanar <qalipynrr@gmail.com>
2024-07-31 17:28:25 +0300

    ürün aktif/deaktif etme hatası düzenlendi

--- src/main/java/com/example/E_commerce/repository/ProductRepository.java
+++ src/main/java/com/example/E_commerce/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package com.example.E_commerce.repository;
 
 import com.example.E_commerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +17,8 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
     @Query("SELECT p FROM Product p WHERE p.categoryId = categoryId")
     List<Product> getProductListByCategoryId(@Param("categoryId") Long categoryId);
 
+    @Transactional
+    @Modifying
     @Query("UPDATE Product p SET p.active = :active WHERE p.id = :id")
     void updateProductActive(@Param("active") Boolean isActive, @Param("id") Long id);
 }
